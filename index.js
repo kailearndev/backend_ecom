66dfec893878ad41b42a862de726ce2cdb00a271
kaidev <kaidev.contact@gmail.com>
2025-01-11 07:11:48 +0700

    cors

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ var corsOptions = {
 // Create a rate limiter with express-rate-limit
 const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per windowMs
+    max: 1000, // Limit each IP to 100 requests per windowMs
     message: 'Too many requests from this IP, please try again later.',
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
